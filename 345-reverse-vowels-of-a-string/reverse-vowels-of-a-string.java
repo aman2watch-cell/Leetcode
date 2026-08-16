6ce303306fe752c7d1da8d840ba7978078b5fe7d
aman2watch-cell <aman2watch@gmail.com>
2026-08-16 10:44:18 +0530

    Time: 4 ms (31.12%) | Memory: 45.9 MB (95.06%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,32 +1,36 @@
 class Solution {
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'A' ||
+               c == 'e' || c == 'E' ||
+               c == 'i' || c == 'I' ||
+               c == 'o' || c == 'O' ||
+               c == 'u' || c == 'U';
+    }
+
     public String reverseVowels(String s) {
         StringBuilder s1 = new StringBuilder(s);
         int n = s1.length();
         int i = 0;
-        int j = n-1;
-            while(i<j){
+        int j = n - 1;
 
-                if((s1.charAt(i)=='a'|| s1.charAt(i)=='A'||s1.charAt(i)=='e'||s1.charAt(i)=='E'||s1.charAt(i)=='i'||s1.charAt(i)=='I'||s1.charAt(i)=='o'||s1.charAt(i)=='O'||s1.charAt(i)=='u'||s1.charAt(i)=='U') &&(s1.charAt(j)=='a'|| s1.charAt(j)=='A'||s1.charAt(j)=='e'||s1.charAt(j)=='E'||s1.charAt(j)=='i'||s1.charAt(j)=='I'||s1.charAt(j)=='o'||s1.charAt(j)=='O'||s1.charAt(j)=='u'||s1.charAt(j)=='U')){
-                    char temp  = s1.charAt(i);
-                    s1.setCharAt(i,s1.charAt(j));
-                    s1.setCharAt(j,temp);
-                    i++;
-                    j--;
+        while (i < j) {
 
+            if (isVowel(s1.charAt(i)) && isVowel(s1.charAt(j))) {
+                char temp = s1.charAt(i);
+                s1.setCharAt(i, s1.charAt(j));
+                s1.setCharAt(j, temp);
+                i++;
+                j--;
 
-                }
-                else if((s1.charAt(i)!='a'&& s1.charAt(i)!='A'&&s1.charAt(i)!='e'&&s1.charAt(i)!='E'&&s1.charAt(i)!='i')&&(s1.charAt(i)!='I'&&s1.charAt(i)!='o'&&s1.charAt(i)!='O'&&s1.charAt(i)!='u'&&s1.charAt(i)!='U')){
-                    i++;
-                }
-                else if((s1.charAt(j)!='a'&& s1.charAt(j)!='A'&&s1.charAt(j)!='e'&&s1.charAt(j)!='E'&&s1.charAt(j)!='i')&&(s1.charAt(j)!='I'&&s1.charAt(j)!='o'&&s1.charAt(j)!='O'&&s1.charAt(j)!='u'&&s1.charAt(j)!='U')){
-                    j--;
-                }
+            } else if (!isVowel(s1.charAt(i))) {
+                i++;
 
+            } else if (!isVowel(s1.charAt(j))) {
+                j--;
             }
-            return s1.toString();
+        }
 
-        
-       
-        
+        return s1.toString();
     }
 }
