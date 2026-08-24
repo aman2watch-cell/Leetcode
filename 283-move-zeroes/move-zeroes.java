0c585bc0ee836fa2e1d342dbdf747c4dc04b4607
aman2watch-cell <aman2watch@gmail.com>
2026-08-24 17:09:13 +0530

    Time: 4 ms (9.39%) | Memory: 47.6 MB (88.49%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0 ;
+        int j = 1;
+        while(i<=j && j<=nums.length-1){
+            if(nums[i]==0 && nums[j]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+                i++;
+
+            
+            }else if(nums[i] ==0 && nums[j]==0){
+                j++;
+           }
+
+           ///else if(nums[i] != 0){
+             //   i++;
+              
+          
+            
+          // }
+           else if(nums[j] != 0 && nums[i]!=0){
+            j++;
+            i++;
+           }else if(nums[i]!=0){
+            i++;
+           }
+
+        }
+        
+        
+    
+    }
+}
