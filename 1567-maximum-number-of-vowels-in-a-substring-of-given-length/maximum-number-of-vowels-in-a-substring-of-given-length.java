4bc56e29f371763c80db9d8f076bcb2ca73980c7
aman2watch-cell <aman2watch@gmail.com>
2026-08-30 11:45:58 +0530

    Time: 14 ms (49.49%) | Memory: 46.3 MB (65.96%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -4,36 +4,23 @@ class Solution {
                 
     }
     public int maxVowels(String s, int k) {
-
-       // StringBuilder sb2 = new StringBuilder(s);
-        //int left = 0;
+        int left = 0;
         int count = 0;
         int max = Integer.MIN_VALUE;
-        StringBuilder sb1 = new StringBuilder();
-        for(int right = 0;right<=s.length()-1;right++){
-            sb1.append(s.charAt(right));
+        for(int right = 0; right<= s.length()-1;right++){
             if(isVowel(s.charAt(right))){
-                    count = count+1;
+                count ++;
             }
-
-            if(sb1.length() == k){
-                max = Math.max(max,count);
-                if(isVowel(sb1.charAt(0))){
-                    count = count-1;
-
+            if(right-left+1==k){
+                max = Math.max(count,max);
+                if(isVowel(s.charAt(left))){
+                    count = count -1;
                 }
-                sb1.deleteCharAt(0);
-              //  left++;
-               
-                       
-
-                    
-                    
-                   
+                left++;
             }
-
         }
         return max;
-        
     }
 }
+
+   
