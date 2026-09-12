c25a6a8ac9410c104534f532dbe31068758ba618
aman2watch-cell <aman2watch@gmail.com>
2026-09-12 08:14:30 +0530

    Time: 1 ms (96.47%) | Memory: 47.3 MB (59.87%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,28 +1,35 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int[] prefix = new int[nums.length];
+        prefix[0]= nums[0];
+        for(int i = 1;i<nums.length;i++){
+            prefix[i] = prefix[i-1]+nums[i];
+        }
+
         int leftsum = 0;
         int rightsum = 0;
-        for(int i = 0 ;i<nums.length;i++){
+        for(int i = 0 ;i<prefix.length;i++){
             leftsum = 0;
             rightsum = 0;
             
             
-            if(i==nums.length-1){
+
+            if(i==0){
+                leftsum = 0;
+            }else{
+
+            
+                
+                leftsum = prefix[i-1];
+            }
+            
+            if(i ==nums.length-1){
                 rightsum = 0;
-            }
-            for(int j = 0;j<i;j++){
-                if(i==0){
-                    leftsum = 0;
-                }
+            }else{
+
+            
                 
-                leftsum = leftsum + nums[j];
-            }
-            for(int k = i+1;k<nums.length;k++){
-                if(i ==nums.length-1){
-                    rightsum = 0;
-                }
-                
-                rightsum = rightsum+nums[k];
+                rightsum = prefix[prefix.length-1]-nums[i]-leftsum;
             }
             if(leftsum==rightsum){
                 return i;
