88aa8e3f68ea95fddccd4d72faabd4f3883b0cb3
aman2watch-cell <aman2watch@gmail.com>
2026-09-12 08:26:20 +0530

    Time: 4 ms (2.23%) | Memory: 43.6 MB (6.72%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int [] altitude = new int [gain.length+1];
+        altitude [0] = 0;
+        for(int i = 1 ;i<=gain.length;i++){
+            altitude [i]= altitude [i-1] + gain[i-1];
+        }
+        Arrays.sort(altitude);
+        return altitude[altitude.length-1];
+    }
+}
