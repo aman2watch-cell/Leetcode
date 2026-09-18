154fb6d8e61d423ba6296aeb13165bd4ac7211ba
aman2watch-cell <aman2watch@gmail.com>
2026-09-18 09:57:59 +0530

    Time: 3 ms (35.30%) | Memory: 43.6 MB (52.39%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i],1);
+            }
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : map.values()){
+            if(set.contains(x)){
+                return false;
+            }else{
+                set.add(x);
+            }
+
+        }
+        return true;
+    }
+}
